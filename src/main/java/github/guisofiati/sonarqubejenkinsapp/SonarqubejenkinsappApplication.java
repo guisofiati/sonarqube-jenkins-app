c7a785468f5d02bf76cc66342f662b366ba20a2c
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-06-06 10:00:03 -0300

    project created

---
+++ src/main/java/github/guisofiati/sonarqubejenkinsapp/SonarqubejenkinsappApplication.java
@@ -0,0 +1,13 @@
+package github.guisofiati.sonarqubejenkinsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubejenkinsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubejenkinsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/guisofiati/sonarqubejenkinsapp/SonarqubejenkinsappApplicationTests.java
@@ -0,0 +1,13 @@
+package github.guisofiati.sonarqubejenkinsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubejenkinsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
